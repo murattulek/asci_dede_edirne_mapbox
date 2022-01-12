a3d265ff2b843b6d15d975d15a18a62a35721bd3
murattulek <36486289+murattulek@users.noreply.github.com>
2022-01-12 19:48:54 +0300

    config

---
+++ src/config.js
@@ -0,0 +1,234 @@
+var config = {
+    style: 'mapbox://styles/murattulek/ckjjuyujf0le519piqpk2o63g',
+    accessToken: 'pk.eyJ1IjoibXVyYXR0dWxlayIsImEiOiIyOWI3NDhlYWM3MmI0NzMzZDkxNmFmYWMyOGYwNTA2ZiJ9.xNX3bJK-pE-BbBhBHx4kDg',
+    showMarkers: true,
+    markerColor: '#000',
+    theme: 'dark',
+    use3dTerrain: false,
+    title: 'Aşçı Dede - Edirne',
+    subtitle: '',
+    byline: 'Eda Güçlü - Murat Tülek',
+    footer: 'Source: source citations, etc.',
+    chapters: [
+        {
+            id: '',
+            alignment: 'left',
+            hidden: false,
+            title: 'Edirne Aşçı Dede',
+            image: '',
+            description: 'Genel bir açıklama olabilir. Şu tarihte Edirne’ye gelmesi vs.',
+            location: {
+                center: [26.55311, 41.66684],
+                zoom: 12,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '502',
+            alignment: 'left',
+            hidden: false,
+            title: 'Karaağaç Tren İstasyonu',
+            image: '',
+            description: 'Bin üç yüz on dört senesi Rebiülâhirin yirmi altıncı [4 Ekim 1896] ve bin üç yüz on iki senesi Eylülün yirmi ikinci pazar günü akşamı yani Arabînin yirmi yedinci pazartesi gecesi gece isneyn gecesi “Allâhumme salli alâ seyyidinâ Muhammed ” zikriyle ve “Sefer-ilallâh” fikriyle “Fe-firrû ilellâhi” niyetiyle ve kemâl-i aşk u muhabbetle şimendifere ma‘a-evlâd u ayâl birtakım cennet hurileriyle Edirne cennetine ve orada medfûn-ı hâk-i ıtrnâk olan hazerât-ı kirâm makberelerine müteveccihen âzim-i râh-ı hak olduğum bu yüzden ki “et-Tarîku ilallâhi bi-adedi enfusi’l halâyık”tır azizim. İşte bu neşe ve bu muhabbetle gece saat sekiz raddelerinde cümleten Edirne istasyonuna nüzul ettik. (vol. II, 945)',
+            location: {
+                center: [26.52154, 41.65161],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: '510',
+            alignment: 'right',
+            hidden: false,
+            title: 'Muradiye Cami',
+            image: './images/TASUH0433.jpg',
+            description: 'İşte bu pazarlıklar sayesi olmalı ki bir cuma günü Muradiye Câmi-i şerîfi’nde cuma namazı eda ve Hazret-i Şeyh Enis kuddise sırruhu’l-azîz ziyaretiyle müşerref olunduktan sonra kabristanın önünden ve cümle kapısından yirmi otuz hatve aşağıda ve yolun karşı tarafında, yerden yapma yani fevkânîsi yoktur, ahır şeklinde bir hane gözüme göründü. (vol. II, 950)',
+            location: {
+                center: [26.56479, 41.68242],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '503',
+            alignment: 'right',
+            hidden: false,
+            title: 'Mevlevihane',
+            image: './images/TASUH0398.jpg',
+            description: 'Bir müddetçik ârâm edip oradan birlikte daireye gitmezden evvelce dâ’ire-i âlî-i Cenâb-ı Mevlânâ olan Mevlevîhaneye ve hâk-i pây-i Hazret-i Şeyh-i Ekrem Hacı Eşref Efendi hazretlerine yüz ve göz sürüp yüzümü şems ziyasıyla pür-nûr ve gözümü kibrît-i ahmer olan kuhl-i Mevlânâ ile tezyin ettim. Hazret-i şeyhin cemaline, kemaline muhabbet ve irfanına sad hezâr aferin ü tahsin edip kapısına halka be-gûş gulâm oldum. (vol. II, 945-6)',
+            location: {
+                center: [26.56438, 41.68274],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '505',
+            alignment: 'left',
+            hidden: false,
+            title: 'Daire-i Vilayet',
+            image: './images/Edirne-Hukumet Konagi.jpg',
+            description: 'Oradan bu cemâ’at-i âlînin muktedâ-bih ve imamı olan zât-ı âlî-i müşîrî devletlû Arif Paşa hazretlerinin hâk-i pây-i hidiv-i a’zamîlerine yüz sürmek üzere vilâyet-i vâlî-i vekâlet-i aliyyesi üzerlerinde olduğundan arabaya râkiben dâ’ire-i vilâyete gittik. Huzûr-ı lâmi‘ü’n-nûr-ı müşîrânelerine kemâl-i edeb ü huzûr ile girmek manası gönül âlemine atıldı. Öylece girdim. Zât-ı âlî-i müşîrîyi kıyamda buldum. (vol. II, 946)',
+            location: {
+                center: [26.55133, 41.67947],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '506',
+            alignment: 'left',
+            hidden: false,
+            title: 'The First House That Aşçı Dede Lived in Edirne',
+            image: '',
+            description: 'Hakk-ı müsâferet üç gün olup bizim Hâlid Efendi’nin hanesinde ikame olundu. Badehu ittisalindeki ve bundan iki ay evvel icar olunmuş olan Üçüncü Şube Müdür Muavini Rasim Efendi’nin nakil resmini tersim ile eski hamam eski tas diyerek mine’l-kadîm melûf olduğum huzur ve ezkâr ile dem-güzâr oldum. (vol. II, 947)',
+            location: {
+                center: [26.56628, 41.67861],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '509',
+            alignment: 'right',
+            hidden: false,
+            title: 'Atik Camii/Eski Cami',
+            image: './images/AHEDI047.jpg',
+            description: 'Edirne’ye geldiğimde teberrüken cuma namazları birer câmi-i şerîfte eda olunup vaktaki Atik Câmi-i şerîfe nöbet geldi, orasını fevkalâde ruhaniyetli bir başka neşe ve tecelliyât zuhur ederek bunun ledünniyâtını taharri ettim. Şöyle buldum ki bu câmi-i şerîfi Edirne’nin feth ü küşadında bina ve inşa olunup pek büyük zevât-ı kirâm hazerâtı bunda ibadet ve tâat ederek çok dua ve niyazlar olunmuş. Hatta Hacı Bayram-ı Velî kuddise sırruhu’l-âlî hazretlerinin makamı olup kürsüleri dahi elyevm mevcuttur. (vol. II, 949)',
+            location: {
+                center: [26.55578, 41.67664],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '512',
+            alignment: 'left',
+            hidden: false,
+            title: 'Selimiye Camii',
+            image: './images/Krt_005139-Selimiye Camii.jpg',
+            description: 'Bu ramazân-ı şerîfte bir gece teravih namazında Sultan Selim Câmi‘-i şerîfi’nde bizim hem-dem ve hem-esrârımız olan ihvân-ı tarîkat-ı aliyyeden Matbaa Memuru Yüzbaşı Hacı Mehmed Emin Efendi ile birleşip bade’s-salât camiden çıkıp mumaileyh fakir gibi olmayıp zâhir ü bâtın kulağı küşade ve hem de ilm-i siyâsede mâhir-i bâhir olup ahvâl-i âlemden sual ettim. (vol. II, 974)',
+            location: {
+                center: [26.55918, 41.67814],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '519',
+            alignment: 'left',
+            hidden: false,
+            title: 'The Third and the Last House That Aşçı Dede Lived in Edirne',
+            image: '',
+            description: 'Binaenaleyh cuma günü alessabah Edirne İstasyonu’na bi’s-sıhhat ve’l-âfiyet çıkıp oradan bir arabaya râkiben “Küçükpazar’da Arnavut Bayırı’nda Asım Efendi’nin hanesine arabacı çek bizi, götür!” dedik. Gidiyoruz, Sultan Selim Câmi’-i şerîfi önünden geçip mekteb-i idâdiye karşısına gelir gelmez, bir de onu gördüm ki mumaileyh Asım Efendi oğlumuz arabanın önüne haramîler gibi çıkıp “Arabacı çevir arabayı buraya!” diyerek sabık Matbaa Memuru Dağıstanî Yüzbaşı bizim Hacı Mehmed Efendi merhumun köşebaşında olan hanesi önünden çevirip o büyük caddeden sokağın içerisine soktu ve hemen orada bir hanenin önünde arabayı durdurup buyurun dedi. Arabadan aşağıya indim. Bir de onu gördüm ki bizim can biraderim, fevkalâde seviştiğim ve Risale’de evvelce zikri sebkat eden mumaileyh Hacı Mehmed Efendi merhumun köşe başında olan hanesine muttasıl kayınvalidesi hanımın hanesi olup şehriye yedi adet mecidiyeye istikra olunmuştur. İşte buna fevkalâde memnun ve mesrur oldum. (vol. III, 1314)',
+            location: {
+                center: [26.56041, 41.67973],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '514',
+            alignment: 'right',
+            hidden: false,
+            title: 'Daire-i Cedide-i Askeriye/Müşir Dairesi',
+            image: './images/Krt_003226-daire-i askeriye.jpg',
+            description: 'Cülûs-ı mes‘adet-me’nûs-ı hazret-i pâdişâhî rûz-ı fîrûzuna müsâdif bin üç yüz on beş senesi Ağustosun on dokuzuncu [31 Ağustos 1899] perşembe günü alessabah dâ’ire-i cedîde-i cenâb-ı müşîr-i efhamîde ümera ve zabitan ve ulemâ-yı kirâm ve meşâyıh-ı zevi’l-ihtirâm ve eşrâf-ı belde hazır oldukları hâlde ed‘iye-i cenâb-ı hilâfet-penâhî yâd u tezkârından sonra dest-i peyvest-i müşîr-i efhamîde olan miftâh-ı tılsım-âsâ ile bizzat kendileri bâb-ı feyz-i necâtı feth ü küşat buyurdular. (vol. II, 1062)',
+            location: {
+                center: [26.55429, 41.67750],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: '222',
+            alignment: 'left',
+            hidden: false,
+            title: 'Edirne Belediyesi',
+            image: './images/AHEDI004.jpg',
+            description: '“İşte belediye dairesi de ism-i celâle mazhar olmuştur, zira orada şehrayinlerde yani şenlik geceleri düvel-i ecnebiye konsoloslarına ziyafet verileceğinden onların usûl ü âdetleri üzere alafranga çalgılar çalınıp familyalarıyla beraber balo usulü dans oynayacaklar, yani ayş u işret ile hora tepecekler ve her türlü fesat ve fısk u fücur icra edecekler. Artık burası dâr-ı cahîm olmaz da dâr-ı na‘îm mi olur azizim? (vol. II, 1064)',
+            location: {
+                center: [26.55864, 41.67961],
+                zoom: 14,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+    ]
+};
